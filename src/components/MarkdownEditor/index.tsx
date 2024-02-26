0b9f1fbe215d755492de85949b81c3d136e2cb7b
calla <sizhu.chen@aelf.io>
2024-02-26 21:09:58 +0800

    feat: markdown editor

---
+++ src/components/MarkdownEditor/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import MdEditor, { Plugins } from 'react-markdown-editor-lite';
+import pinFileToIPFS from 'components/PinFileToIPFS';
+import MarkdownPreview from './MarkdownPreview';
+import 'react-markdown-editor-lite/lib/index.css';
+import './index.css';
+
+MdEditor.unuse(Plugins.Clear);
+MdEditor.unuse(Plugins.FontUnderline);
+
+interface IMarkdownEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function MarkdownEditor({ value, onChange }: IMarkdownEditorProps) {
+  const handleEditorChange: MdEditor['props']['onChange'] = ({ text }) => {
+    onChange(text);
+  };
+
+  const handleImageUpload: Required<MdEditor['props']>['onImageUpload'] = async (file: File) => {
+    try {
+      const uploadData = await pinFileToIPFS(file);
+      return uploadData?.url ?? '';
+    } catch (error) {
+      console.log('uploadData error: ', error);
+      return '';
+    }
+  };
+
+  return (
+    <MdEditor
+      className="markdown-editor"
+      placeholder="Please input the content"
+      view={{
+        menu: true,
+        md: true,
+        html: false,
+      }}
+      renderHTML={(text) => <MarkdownPreview text={text} />}
+      value={value}
+      onChange={handleEditorChange}
+      onImageUpload={handleImageUpload}
+    />
+  );
+}
