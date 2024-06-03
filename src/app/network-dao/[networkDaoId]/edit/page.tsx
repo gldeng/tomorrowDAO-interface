5394e2e87072ddb00cb7c26480477a1cd9b5c10c
currymz <currymz@tmrwdao.com>
2024-06-03 19:35:46 +0800

    feat: remove network-dao edit page loading

--- src/app/network-dao/[networkDaoId]/edit/page.tsx
+++ src/app/network-dao/[networkDaoId]/edit/page.tsx
@@ -1,11 +1,9 @@
 'use client';
 import React from 'react';
-import { SkeletonList } from 'components/Skeleton';
 
 import dynamicReq from 'next/dynamic';
 const PageIndex = dynamicReq(() => import('./_page'), {
     ssr: false,
-    loading: () => <SkeletonList />,
 });
 
 export default function Page() {
