ac8c089cd4c6afa1d28e6bad0311afc58ef979dc
currymz <currymz@tmrwdao.com>
2024-07-23 15:51:28 +0800

    feat: proposal deploy token list

--- src/app/proposal/deploy/[aliasName]/DeployForm/ProposalInfo.tsx
+++ src/app/proposal/deploy/[aliasName]/DeployForm/ProposalInfo.tsx
@@ -34,7 +34,7 @@ interface ProposalInfoProps {
   onSubmit: () => void;
   onTabChange?: (activeKey: string) => void;
   activeTab?: string;
-  treasuryAssetsData?: IAddressTokenListDataItem[];
+  treasuryAssetsData?: ITreasuryAssetsResponseDataItem[];
   daoDataLoading?: boolean;
 }
 
