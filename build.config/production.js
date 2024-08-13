6d4d9109e21b7ff11de66f83ba1920a0c3318157
currymz <currymz@tmrwdao.com>
2024-08-13 22:16:42 +0800

    feat: delete useless build config

--- build.config/production.js
+++ build.config/production.js
@@ -2,14 +2,5 @@ const commonConfig = require('./common');
 module.exports = {
   ...commonConfig,
   swcMinify: true,
-  experimental: {
-    'react-use': {
-      transform: 'react-use/lib/{{member}}',
-    },
-    lodash: {
-      transform: 'lodash/{{member}}',
-    },
-  },
   productionBrowserSourceMaps: false,
-  resolve: {},
 };
