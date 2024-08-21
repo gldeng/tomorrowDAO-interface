9cdd573522487ea55cef3066109d90d8c3e7e7dd
currymz <currymz@tmrwdao.com>
2024-08-21 22:09:28 +0800

    feat: vote item split 3 layers

--- src/app/telegram/votigram/components/VoteItem/index.tsx
+++ src/app/telegram/votigram/components/VoteItem/index.tsx
@@ -65,6 +65,7 @@ export default function VoteItem(props: IVoteItemProps) {
         )}
       </div>
       {!canVote && <Percent percent={item.votePercent} />}
+      <div className="background"></div>
     </div>
   );
 }
