a3bfa6581b341e1b2a43dfb98488e8bed5358c6c
currymz <currymz@tmrwdao.com>
2024-09-26 23:24:06 +0800

    feat: convert start source to utm source

--- src/app/GtagConfigScript.tsx
+++ src/app/GtagConfigScript.tsx
@@ -7,6 +7,7 @@ export default function VconsoleScript() {
   return (
     <Script>
       {`
+      function main() {
         var urlObj = new URL(window.location.href);
         var utmSource = urlObj.searchParams.get('utm_source');
         var utmMedium = urlObj.searchParams.get('utm_medium');
@@ -24,10 +25,36 @@ export default function VconsoleScript() {
         if (utmTerm) campaign.term = utmTerm;
         if (utmContent) campaign.content = utmContent;
 
+        if (!utmSource && window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe && window.Telegram.WebApp.initDataUnsafe.start_param) {
+            var start_param = window.Telegram.WebApp.initDataUnsafe.start_param;
+            var AND_CHAR = '_';
+            var CONNECT_CHAR = '-';
+            const parseStartAppParams = (params) => {
+                const result = {};
+                const parts = params.split(CONNECT_CHAR);
+
+                for (const part of parts) {
+                    const [key, value] = part.split(AND_CHAR);
+                    if (key && value !== undefined) {
+                    result[key] = value;
+                    }
+                }
+                return result;
+            };
+            const params = parseStartAppParams(start_param);
+            if (params.source) campaign.source = params.source;
+        }
+
 
         console.log('campaign params', campaign);
         if (Object.keys(campaign).length > 0) {
             gtag('set', 'campaign', campaign);
+        }
+      }
+        try {
+            main();
+        } catch (e) {
+            console.log('error in vconsole script', e);
         }
         `}
     </Script>
