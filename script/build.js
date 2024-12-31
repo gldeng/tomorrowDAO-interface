19b9adee3dce1149ca919ab58650e017d277ddf9
gldeng <realgldeng@gmail.com>
2024-12-31 18:31:30 +0800

    Add kurtosis enclave build config

--- script/build.js
+++ script/build.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process');
 const fs = require('fs');
+const publicIp = require('public-ip');
+
 // const { getConfig, getContractAddress } = require('./generate-config/queryConfig');
 
 const APP_ENV = process.env.APP_ENV || 'testnet';
@@ -16,6 +18,27 @@ async function main() {
     );
     fs.writeFileSync('./src/config/index.ts', newFileContent);
     console.log(`APP_ENV: ${APP_ENV}, replace testnet with mainnet !!!`);
+    // await Promise.all([getConfig(), getContractAddress()]);
+  }
+  if (APP_ENV === 'kurtosisenclave') {
+    const fileConfigContent = fs.readFileSync('./src/config/index.ts', 'utf-8');
+    let kurtosisenclaveImportStatement = `export * from './kurtosisenclave';`;
+    let testnetImportStatement = `export * from './testnet';`;
+    const newFileContent = fileConfigContent.replace(
+      testnetImportStatement,
+      kurtosisenclaveImportStatement,
+    );
+    fs.writeFileSync('./src/config/index.ts', newFileContent);
+    console.log(`APP_ENV: ${APP_ENV}, replace testnet with kurtosisenclave !!!`);
+
+    const ip = await publicIp.v4();
+    const originalKurtosisEnclave = fs.readFileSync('./kurtosisenclave', 'utf-8');
+    const newKurtosisEnclave = originalKurtosisEnclave.replace(
+      '__PUBLIC_IP__PLACEHOLDER__',
+      ip
+    );
+    fs.writeFileSync('./kurtosisenclave', newKurtosisEnclave);
+    console.log(`Replaced __PUBLIC_IP__PLACEHOLDER__ with ${ip} in ./kurtosisenclave`);
     // await Promise.all([getConfig(), getContractAddress()]);
   }
   const buildCommand = spawn('yarn', ['next-compile']);
---
+++ src/config/kurtosisenclave.ts
@@ -0,0 +1,65 @@
+// static config for disable label value todo...
+export const electionContractAddress = 'ugE15RDZs36kutC5yLdR2iGC2D7o5WFW98vDvJ76FJcYDKCbz';
+// for network-dao treasury account
+export const treasuryAccountAddress = '7VdDpXgZd7PDQf4vRSZMy6nLukAviDAyHDs87o7ZUM9FbQQsA';
+
+// ------------------aelf-web-login-start------------------------
+export const networkType = 'TESTNET';
+export const aelfWebLoginNetworkType = 'TESTNET';
+export const rpcUrlAELF = "http://__PUBLIC_IP__PLACEHOLDER__:8000";
+export const rpcUrlTDVV = "http://__PUBLIC_IP__PLACEHOLDER__:8000";
+export const rpcUrlTDVW = "http://__PUBLIC_IP__PLACEHOLDER__:8000";
+export const txRelayerUrl = "http://__PUBLIC_IP__PLACEHOLDER__:8020";
+export const connectServer = 'https://auth-portkey-test.portkey.finance';
+export const graphqlServer =
+  'http://35.232.58.162:34183/tomorrowdao_indexer/d736f750b23746beae11d22e1e0ff78a/graphq';
+export const portkeyServer = 'https://aa-portkey-test.portkey.finance';
+export const connectUrl = 'https://auth-aa-portkey-test.portkey.finance';
+
+export const TELEGRAM_BOT_ID = '7190916077';
+// ------------------aelf-web-login-end------------------------
+
+export const curChain = 'AELF';
+// system contract： token contract address
+export const mainChainAddress = 'JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE';
+export const sideChainAddress = 'ASh2Wt7nSEmYqnGxPPzp4pnVDU4uhj1XW9Se5VeZcX2UDdyjx';
+export const sideChainCAContractAddress = '238X6iw1j8YKcHvkDYVtYVbuYk2gJnK8UoNpVCtssynSpVC8hb';
+// tmrw contract: biz contract
+export const daoAddress = 'defdwNRez3U8WpdVE73p6xuiixeYYJ9hn9ZS5GMSpTHbJ2omt';
+export const propalAddress = 'j5c9dv4ird82fbcBiaHvBys4m3qZFaYnxPKUgDpkhc7nsbvaa';
+export const voteAddress = 'NCi6nskU1A3QTNxcxgo1rnrrtkhVh56jXqj4m2T6uXs5hjsSF';
+export const treasuryContractAddress = '7VdDpXgZd7PDQf4vRSZMy6nLukAviDAyHDs87o7ZUM9FbQQsA';
+// explorer config
+export const explorer = 'https://testnet.aelfscan.io/tDVW';
+export const mainExplorer = 'https://testnet.aelfscan.io/AELF';
+
+// network-dao contract address: get network dao highCouncil table
+export const electionContractAddr = 'NrVf8B7XUduXn1oGHZeF1YANFXEXAhvCymz2WPyKZt4DE2zSg';
+export const consensusDPoSAddr = 'pGa4e5hNGsgkfjEGm72TEvbF7aRDqKBd4LuXtab4ucMbXLcgJ';
+
+export const sideChainSuffix = 'tDVW';
+// symbolmarket page url
+export const symbolmarketUrl = 'https://test.eforest.finance/symbolmarket';
+
+// explorer
+export const SOCKET_URL_NEW = 'wss://explorer-test.aelf.io';
+
+// header
+export const chainIdSelect = [
+  {
+    value: 'AELF',
+    label: 'MainChain AELF',
+  },
+];
+
+// network-dao
+export const networkDaoId = 'b2dcb3684b00e857983f7b75917682a1d3b2f175d668e77de22743e08f6c3fed';
+
+// website
+export const host = 'https://test.tmrwdao.com';
+
+// telegram
+export const nftSymbol = 'TOMORROWPASSTEST-1';
+
+// api
+export const apiServerBase = "http://__PUBLIC_IP__PLACEHOLDER__:8010";
